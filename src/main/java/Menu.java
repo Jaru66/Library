5c8cd08d91087714b6d5afb83c504471343fcdfc
Jarek <jareko22@gmail.com>
2022-05-31 18:48:57 +0200

    new options in menu

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -6,25 +6,37 @@ public class Menu {
     private static void printMenu(){
         System.out.println("1. Dodaj książkę do księgozbioru");
         System.out.println("2. Dodaj użytkownika");
-        System.out.println("3. ");
-        System.out.println("4. ");
-        System.out.println("5. ");
+        System.out.println("3. Dodaj ksiazke do usera tymczasowo");
+        System.out.println("4. Wyswietl liste ksiazek");
+        System.out.println("5. Wyswietl liste userow");
+        System.out.println("6. Wyswietl ksiazki usera");
     }
     public static void selectMenu(){
         printMenu();
         selectedOption = scanner.nextInt();
         while(true){
             switch (selectedOption){
-                    case 1:BooksManagement.addNew("tytl","auth");
+                    case 1:BooksManagement.addNew(BooksManagement.askForTitle(),BooksManagement.askForAuthor());
                     break;
-                    case 2:CustomerManagement.addNew("s","s");
+                    case 2:CustomerManagement.addNew(CustomerManagement.askForName(),CustomerManagement.askForSurname());
                     break;
-                    case 3:CustomerManagement.addBookToCustomerArrayList(CustomerManagement.getCustomer(1),0);
+                    case 3:
+                        System.out.println("Customer,bookID");CustomerManagement.addBookToCustomerArrayList(CustomerManagement.getCustomer(scanner.nextInt()),scanner.nextInt());
                     break;
                     case 4:
-                        System.out.println(BooksManagement.getBookName(0));
+                        for(int i=0;i<BooksManagement.getLibrarySize();) {
+                            System.out.println(BooksManagement.getBook(i).id+" "+BooksManagement.getBookName(i));
+                            i++;
+                        }
                     break;
                     case 5:
+                        for (int o=0;o<CustomerManagement.getCustomersSize();o++){
+                            System.out.println(CustomerManagement.getCustomer(o).id+" "+CustomerManagement.getCustomerName(o));
+                        }
+                    break;
+                    case 6:
+                        System.out.println("Wpisz ID usera ktorego ksiazki chcesz wyswietlic");
+                         CustomerManagement.getCustomer(scanner.nextInt()).printBorrowedBooks();
                     break;
             }
             Menu.selectMenu();
