d0086c209b0fe0d7d50fb89c40c50d206d34749d
Jarek <jareko22@gmail.com>
2022-06-16 19:55:34 +0200

    added some finally connection close to methods

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -29,20 +29,19 @@ public class Database {
             connect();
             statement = connection.createStatement();
             String sql = "SELECT * FROM books ;";
-
             ResultSet rs = statement.executeQuery(sql);
             while (rs.next()) {
-
                 BooksManagement.addNew(rs.getInt("id"),rs.getString("title"),rs.getString("author"),rs.getBoolean("isAvaliable"));
                 int id = rs.getInt("id");
                 String title = rs.getString("title");
                 String author = rs.getString("author");
                 Boolean isAvaliable = rs.getBoolean("isAvaliable");
 
-
                 System.out.println(id + " " + title+ "       ["+ author+"]"+isAvaliable);
             }
-        }catch (Exception e) {e.printStackTrace();}
+        }catch (Exception e) {e.printStackTrace();}finally {
+            connection.close();
+        }
     }
 
     //at program close
@@ -52,16 +51,12 @@ public class Database {
             statement = connection.createStatement();
             String sql = null;
             for(int i=0;i<BooksManagement.getLibrarySize();i++) {
-
                 sql = "INSERT INTO `books` (`id`,`title`,`author`,`isAvaliable`) VALUES ('"+BooksManagement.getBook(i).id+ "','"+BooksManagement.getBook(i).title+"','"+BooksManagement.getBook(i).author+"','"+return1WhileTrue(BooksManagement.getBook(i).isAvaliable)+"');";
                 if(bookExists(i)){
                     if(BooksManagement.getBook(i).isAvaliable!=getBookAvailability(i)){
                         System.out.println("jest niedostepna");sql = "UPDATE books SET isAvaliable = '"+return1WhileTrue(BooksManagement.getBook(i).isAvaliable)+"' WHERE id = '"+i+"';";statement.executeUpdate(sql);}
                 }else statement.executeUpdate(sql);
-
             }
-
-
         } catch (Exception e){e.printStackTrace();}finally {
             connection.close();
         }
@@ -73,11 +68,8 @@ public class Database {
     public static void downloadCustomerBookList(Customer customer){    }
     public static void uploadCustomerBookList(Customer customer){    }
 
-    public static int return1WhileTrue(boolean bool){
-        if (bool) {return 1;}
-        else return 0;
-    }
-    public static boolean bookExists(int id){
+    public static int return1WhileTrue(boolean bool){        if (bool) {return 1;}        else return 0;    }
+    public static boolean bookExists(int id) throws SQLException {
         try {
             connect();
             statement = connection.createStatement();
@@ -85,10 +77,12 @@ public class Database {
             ResultSet rs = statement.executeQuery(sql);
             while (rs.next()) {return true;
             }
-        }catch (Exception e) {e.printStackTrace();}
+        }catch (Exception e) {e.printStackTrace();}finally {
+            connection.close();
+        }
         return false;
     }
-    public static boolean getBookAvailability(int    id){
+    public static boolean getBookAvailability(int    id) throws SQLException {
         try {
             connect();
             statement = connection.createStatement();
@@ -96,9 +90,10 @@ public class Database {
             ResultSet rs = statement.executeQuery(sql);
             while (rs.next()) {
                 return rs.getBoolean("isAvaliable");
-
             }
-        }catch (Exception e) {e.printStackTrace();}
+        }catch (Exception e) {e.printStackTrace();}finally {
+            connection.close();
+        }
         return false;
     }
 }
